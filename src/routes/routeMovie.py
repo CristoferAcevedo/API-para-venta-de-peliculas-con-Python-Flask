5de23f7623f168b8fa29086e327337c1245ccdd9
CristoferAcevedo <158364057+CristoferAcevedo@users.noreply.github.com>
2025-06-23 16:17:33 -0500

    se acomodo la ruta para obtener una sola pelicula a traves del id y se esta acomodando la ruta para crear peliculas

--- src/routes/routeMovie.py
+++ src/routes/routeMovie.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify,request
 
 #modelos
 from models.movieModel import MovieModel
@@ -10,5 +10,29 @@ def get_movies():
     try:
         movies=MovieModel.get_movies()
         return jsonify(movies)
+    except Exception as ex:
+        return jsonify({'message': str(ex)}),500
+    
+@main.route('/<id>')
+def get_movie(id):
+    try:
+        movie=MovieModel.get_movie(id)
+        if movie == None:
+            return jsonify({}),404
+        
+        return jsonify(movie)
+    except Exception as ex:
+        return jsonify({'message': str(ex)}),500
+    
+@main.route('/add',methods=['POST'])
+def add_movie():
+    try:
+
+        
+        movie=MovieModel.add_movie(movie)
+        if movie == None:
+            return jsonify({}),404
+        
+        return jsonify(movie)
     except Exception as ex:
         return jsonify({'message': str(ex)}),500
